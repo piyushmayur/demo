07d28d9bf815f95b141255e072dc69574db842e3
Group058 <Group058@infoway.com>
2022-11-10 03:47:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Calculator from './Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="app">
+        <h1>React Calculator App</h1>
+        <Calculator />
+        <style global jsx>{`
+          * {
+            box-sizing: border-box;
+          }
+
+          html,
+          body {
+            height: 100%;
+          }
+
+          body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Roboto', sans-serif;
+            min-height: 100%;
+            background: #ecf0f1;
+            color: #2c3e50;
+          }
+
+          .app {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 30px auto;
+          }
+
+          .app h1 {
+            text-transform: uppercase;
+            font-weight: 600;
+          }
+        `}</style>
+      </main>
+    );
+  }
 }
 
 export default App;
